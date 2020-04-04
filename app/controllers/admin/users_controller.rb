adc0452b2ee4b5557fd43159c76998106694f453
fujisawayoshitaka <1317699@gmail.com>
2020-04-04 17:09:15 +0900

    login機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,13 +14,39 @@ class Admin::UsersController < ApplicationController
   end
 
 
-  
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confimation)
+  end
+
 end
